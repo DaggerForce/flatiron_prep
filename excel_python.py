6c6f99db263c9ffbe4adaa3e962f559611967ff0
Gal Gilor <gilorgal2010@gmail.com>
2019-04-09 18:58:24 -0400

    complete

--- excel_python.py
+++ excel_python.py
@@ -1,9 +1,12 @@
+# pip install pandas
+# python -m pip install --upgrade pip
 import pandas
 travel_df = pandas.read_excel('./cities.xlsx')
 cities = travel_df.to_dict('records')
 cities[0]
-list(cities[0].keys()) # Note that the `keys()` function returns a `dict_keys` object. It's a little tricky to work with that type of object, so let's coerce it into a list.
-list(cities[0].values()) # similar to the 'keys' this is the value of each key
+# Note that the `keys()` function returns a `dict_keys` object. It's a little tricky to work with that type of object, so let's coerce it into a list.
+list(cities[0].keys())
+list(cities[0].values())  # similar to the 'keys' this is the value of each key
 
 # philadelphia = {'City': 'Philadelphia'} - how we learnt to create dictionarys
 # pittsburgh = dict(city='Pittsburgh')- another possible way.
