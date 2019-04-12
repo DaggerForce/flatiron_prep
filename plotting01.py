faccac6c6ce354e23d0d931c17aeb9af9f808d7b
Gal Gilor <gilorgal2010@gmail.com>
2019-04-12 12:34:31 -0400

    still working

--- plotting01.py
+++ plotting01.py
@@ -1,4 +1,9 @@
 import plotly
 
+
 plotly.offline.init_notebook_mode(connected=True)
 # use offline mode to avoid initial registration
+
+
+trace0 = {'type': 'bar', 'x': ['jack', 'jill', 'sandy'], 'y': [8, 11, 10]}
+trace0
